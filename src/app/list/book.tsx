30ff2544853c3e678aa6f978da0d5e33e3d8c341
Simon Müller <simon.mueller@l-mobile.com>
2024-09-30 13:08:24 +0200

    make whole listitem clickable

--- src/app/list/book.tsx
+++ src/app/list/book.tsx
@@ -15,20 +15,18 @@ interface Book {
 export default function Book({...params}: Book)
 {
 	return (
-		// make the cover image its own component?
-		<li key={params.isbn} className="flex justify-between gap-x-6 py-5">
-			
-			<Link href={`/details/${params.isbn}/`}>GO TO DETAILS: {params.isbn}</Link>
-			<img alt="" src={params.imageLink}/>
-			<div className="min-w-0 flex-auto">
-				<p className="text-sm font-semibold leading-6 text-gray-900">{params.title}</p>
-				<p className="mt-1 truncate text-xs leading-5 text-gray-500">{params.author}</p>
-			</div>
-			<div className="mt-1 flex items-center gap-x-1.5">
-				<p>{params.releaseDate}</p>
-				<div>{params.availability ? '🟢' : '🔴'}</div>
-			</div>
-		</li>
-		// availability
+		<Link href={`/details/${params.isbn}/`}>
+			<li key={params.isbn} className="flex justify-between gap-x-6 py-5">
+				<img alt="" src={params.imageLink}/>
+				<div className="min-w-0 flex-auto">
+					<p className="text-sm font-semibold leading-6 text-gray-900">{params.title}</p>
+					<p className="mt-1 truncate text-xs leading-5 text-gray-500">{params.author}</p>
+				</div>
+				<div className="mt-1 flex items-center gap-x-1.5">
+					<p>{params.releaseDate}</p>
+					<div>{params.availability ? '🟢' : '🔴'}</div>
+				</div>
+			</li>
+		</Link>
 	)
 }
