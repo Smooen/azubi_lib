947c09902dcc571b73c83acf61c2f24d46a515f6
Simon Müller <simon.mueller@l-mobile.com>
2024-10-07 11:21:31 +0200

    change book testdata, add check if book wasnt found

--- src/app/details/[isbn]/page.tsx
+++ src/app/details/[isbn]/page.tsx
@@ -1,12 +1,18 @@
 import BookDetails from "./details";
 
 export default async function Details({params}: { params: { isbn: string }}) {
-	// just rawdog fetch google books api here with isbn from url?
-	// how the fuck does react-ts typechecking work lmao
-	let book = await fetch('https://www.googleapis.com/books/v1/volumes?q=isbn:978-0-441-17271-9');
+	// don't fetch hardcoded isbn
+	// let book = await fetch('https://www.googleapis.com/books/v1/volumes?q=isbn:978-0-441-17271-9');
+	//let book = await fetch('https://www.googleapis.com/books/v1/volumes?q=isbn:' + params.isbn);
+	let book = await fetch('https://www.googleapis.com/books/v1/volumes?q=isbn' + params.isbn);
 	let content = await book.json();
+	console.log(content);
+	if (content.totalItems == 0) {
+		return (
+			<p className="accent-red-600">No book found on Google Books, ideally redirect to 404</p>
+		)
+	}
 	let volumeInfo = content.items[0].volumeInfo;
-	console.log(content.items[0].volumeInfo)
 	
 	return (
 		<div className="container mx-auto px-4">
